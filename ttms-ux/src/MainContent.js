93379c8212cdaa283d48a903a8b5658b51875822
rony joy <ronyjoy@ronys-mbp.lan>
2020-11-03 21:28:32 -0600

    adding chip

--- ttms-ux/src/MainContent.js
+++ ttms-ux/src/MainContent.js
@@ -7,63 +7,72 @@ import { makeStyles } from '@material-ui/core/styles';
 
 import './css/content.css'
 
+
+import Avatar from '@material-ui/core/Avatar';
+import Chip from '@material-ui/core/Chip';
+import FaceIcon from '@material-ui/icons/Face';
+import DoneIcon from '@material-ui/icons/Done';
+
 const useStyles = makeStyles((theme) => ({
     root: {
-      '& > *': {
-        margin: theme.spacing(1),
-        width: '25ch',
-      },
+        display: 'flex',
+        justifyContent: 'center',
+        flexWrap: 'wrap',
+        '& > *': {
+            margin: theme.spacing(0.5),
+        },
     },
-  }));
-
-function SwitchLabels() {
-  const [state, setState] = React.useState({
-    checkedA: true,
-    checkedB: true,
-  });
-
-
-  const handleChange = (event) => {
-    setState({ ...state, [event.target.name]: event.target.checked });
-  };
-}
-
-
-
+}));
 
 export default function MainContent() {
     const classes = useStyles();
+
+    const handleDelete = () => {
+        console.info('You clicked the delete icon.');
+    };
+
+    const handleClick = (item) => {
+        console.info('You clicked the Chip.'+item);
+    };
+
     return (
-        <div>
-            <div class="container">
-                <div class="col col-6">
-                <form className={classes.root} noValidate autoComplete="off">
-                    <TextField id="standard-basic" label="Player 1 Name" />
-                </form>
+        <div className={classes.root}>
+            <div>
+                <div class="container">
+                    <div class="col col-6">
+                        <form className={classes.root} noValidate autoComplete="off">
+                            <TextField id="standard-basic" label="Player 1 Name" />
+                        </form>
+                    </div>
+                    <div class="col col-6">
+                        <form className={classes.root} noValidate autoComplete="off">
+                            <TextField id="standard-basic" label="Player 2 Name" />
+                        </form>
+                    </div>
+
                 </div>
-                <div class="col col-6">
-                <form className={classes.root} noValidate autoComplete="off">
-                    <TextField id="standard-basic" label="Player 2 Name" />
-                </form>
+                <div class="container">
+                    <div class="player1 col col-6">
+                        <Chip size="small" avatar={<Avatar>M</Avatar>} label="Serve" onClick={()=>handleClick('P1Serve')} />
+                    </div>
+                    <div class="player2 col col-6">
+                        <Chip size="small" avatar={<Avatar>M</Avatar>} label="Serve" onClick={()=>handleClick('p2Serve')} />
+                    </div>
+               
                 </div>
-                
+
             </div>
-            <div class="container">
-                <div class="col col-3">
-                <FormControlLabel control={<Switch />} label="UnderSpin" />
-                </div>
-                <div class="col col-3">
-                    
-                </div>
-                <div class="col col-3">
-                <FormControlLabel control={<Switch />} label="UnderSpin" />
-                </div>
-                <div class="col col-3">.
-                </div>
-            </div>
-    
+
+
         </div>
-    )
-
-
+    );
 }
+
+
+
+
+
+
+
+
+
